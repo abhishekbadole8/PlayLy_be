2684b043d2b233a194f21de2546b4234a9d94ef1
abhishekbadole1 <abhishekb@blockshards.in>
2024-08-15 14:09:25 +0530

    feat: initial commit, mongodb config read

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const connectDb = require("./src/config/dbConnection");
+// const userRoutes = require("./src/routes/userRoutes");
+connectDb();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors()); // CORS middleware
+
+// app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server Listening To Port: ${port}`);
+});
